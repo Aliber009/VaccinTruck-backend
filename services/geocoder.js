7388e49677fdae382c90f3468c2e6b87f44b96e8
Ali Beroro <a.beroro@digieye.io>
2022-03-02 16:13:54 +0100

    update token

--- services/geocoder.js
+++ services/geocoder.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const geocode=async(lat,lng)=>{
     var place=""
-     const address= await axios.get("https://api.openrouteservice.org/geocode/reverse?api_key=5b3ce3597851110001cf624803d896806f2544b6a95312df342a12d7&point.lon="+lng+"&point.lat="+lat)
+     const address= await axios.get("https://api.openrouteservice.org/geocode/reverse?api_key=5b3ce3597851110001cf6248c30d22daa07c488192c7803c2ec18cb6&point.lon="+lng+"&point.lat="+lat)
      const {data}=await address
      place=data.features[0].properties.label;
      return place
